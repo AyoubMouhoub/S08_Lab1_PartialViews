f35ef58ecdded5afebaa8718605fe3c0434c9443
Ayoub Mouhoub <2267937@cegepmontpetit.ca>
2026-09-18 10:36:00 -0400

    ADA
    DADA

---
+++ ZombieParty/ZombieParty/Controllers/HuntingLogController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ZombieParty.Controllers
+{
+    public class MovieController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+
+
+}
